8feba233a76ef014fc0eb54042c79bbaa3a7d807
Viktorian76 <73986593+Viktorian76@users.noreply.github.com>
2023-04-06 03:23:47 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -16,7 +16,7 @@ def solution(p: float, x: np.array) -> tuple:
     #scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
     
     size_x = len(x)
-    sigma_h = np.sqrt(np.sum(X**2) / (31 * size_x))
+    sigma_h = np.sqrt(np.sum(x**2) / (31 * size_x))
     
     left = sigma_h / np.sqrt(chi2.ppf(1 - alpha/2, df=size_x))
     right = sigma_h / np.sqrt(chi2.ppf(alpha/2, df=size_x))
